c1185fb2594c87c5378a78a622c68fe8d9e041e0
gabriel <gabrielcasafu@gmail.com>
2023-11-10 16:08:39 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Display from './components/Display'
+import Buttons from './components/Buttons'
 
-function App() {
+const App = () => {
+  const [result, setResult] = useState("")
+
+  const handleClick = (e) =>{
+    setResult(result.concat(e.target.name))
+  }
+
+  const clear = (e) =>{
+    setResult("")
+  }
+
+  const deleteKey = () =>{
+    setResult(result.slice(0, result.length -1))
+  }
+
+  const calculate = () =>{
+    setResult(eval(result).toString())
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Estándar</h1>
+      <Display 
+        result={result}
+      />
+      <Buttons 
+        handleClick={handleClick} 
+        clear={clear} 
+        deleteKey={deleteKey} 
+        calculate={calculate}
+      />
     </div>
   );
 }
---
+++ src/components/Display.jsx
@@ -0,0 +1,9 @@
+const Display = ({result}) => {
+    return (
+      <form>
+        <input type="text" value={result}/>
+      </form>
+    );
+  }
+  
+  export default Display;
